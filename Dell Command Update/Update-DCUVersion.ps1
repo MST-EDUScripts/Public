a90bc273cc3985ada97ea9257cc2a38e3deafc6b
MST-EDUScripts <63737541+MST-EDUScripts@users.noreply.github.com>
2020-05-21 12:20:04 -0400

    Update Update-DCUVersion.ps1
    Added return values for different situations.

--- Dell Command Update/Update-DCUVersion.ps1
+++ Dell Command Update/Update-DCUVersion.ps1
@@ -1,14 +1,18 @@
+#This script is designed to be used with an RMA, in this case Kaseya.  It will look for an installation of
+#Dell Command update and check the version number, then return a value back to the RMA.
+#The RMA will then perform an action depending on the return value.
+
 $addremovelist = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\windows\CurrentVersion\Uninstall
 $dellCommand = $addremovelist | ForEach-Object {Get-ItemProperty $_.PSPath} | Where-Object DisplayName -like "Dell Command*"
 if ($dellCommand){
     $dellCommandVersion = [version]$dellCommand.DisplayVersion
     if (($dellCommandVersion.Major -eq 3) -and ($dellCommandVersion.minor -lt 1)){
-        return "Need to update!  Running $dellCommandVersion"
+        return "Update"
     }
     if (($dellCommandVersion.Major -eq 3) -and ($dellCommandVersion.Minor -eq 1)){
-        return "Already running $dellcommandversion.  Setting custom options."
+        return "Customize"
     }
 }
 else {
-    return "Dell Command Update not found!"
+    return "Install"
 }
