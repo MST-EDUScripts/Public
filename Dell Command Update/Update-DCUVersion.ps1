44681d2f301e526fea29c21d4ee8192b7b766e04
MST-EDUScripts <63737541+MST-EDUScripts@users.noreply.github.com>
2020-05-21 10:05:43 -0400

    Create Update-DCUVersion.ps1
    Starting with only returning the version number.  Will add more logic once this is verified to work.

---
+++ Dell Command Update/Update-DCUVersion.ps1
@@ -0,0 +1,5 @@
+$addremovelist = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\windows\CurrentVersion\Uninstall
+$dellCommand = $addremovelist | ForEach-Object {Get-ItemProperty $_.PSPath} | Where-Object DisplayName -like "Dell Command*"
+if ($dellCommand){
+    return $dellCommand.DisplayVersion
+}
