12c2b0e5f9806f56d104ee90944526f1c1b21104
MST-EDUScripts <63737541+MST-EDUScripts@users.noreply.github.com>
2020-05-21 11:02:53 -0400

    Update Update-DCUVersion.ps1

--- Dell Command Update/Update-DCUVersion.ps1
+++ Dell Command Update/Update-DCUVersion.ps1
@@ -1,7 +1,13 @@
 $addremovelist = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\windows\CurrentVersion\Uninstall
 $dellCommand = $addremovelist | ForEach-Object {Get-ItemProperty $_.PSPath} | Where-Object DisplayName -like "Dell Command*"
 if ($dellCommand){
-    return $dellCommand.DisplayVersion
+    $dellCommandVersion = [version]$dellCommand.DisplayVersion
+    if (($dellCommandVersion.Major -eq 3) -and ($dellCommandVersion.minor -lt 1)){
+        return "Need to update!  Running $dellCommandVersion"
+    }
+    if (($dellCommandVersion.Major -eq 3) -and ($dellCommandVersion.Minor -eq 1)){
+        return "Already running $dellcommandversion.  Setting custom options."
+    }
 }
 else {
     return "Dell Command Update not found!"
