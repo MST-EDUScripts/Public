3645525146611992bc76110b15cf876e4fb2c05a
MST-EDUScripts <63737541+MST-EDUScripts@users.noreply.github.com>
2020-05-22 15:41:45 -0400

    Update Update-DCUVersion.ps1
    Install location can be in Program Files or Program Files x86, adding more logic to determine install location.

--- Dell Command Update/Update-DCUVersion.ps1
+++ Dell Command Update/Update-DCUVersion.ps1
@@ -3,6 +3,11 @@
 #The RMA will then perform an action depending on the return value. You can run the script with a switch parameter
 #to determine what info is being returned.
 
+#This script is designed to be used with an RMA, in this case Kaseya.  It will look for an installation of
+#Dell Command update and check the version number, then return a value back to the RMA.
+#The RMA will then perform an action depending on the return value. You can run the script with a switch parameter
+#to determine what info is being returned.
+
 [CmdletBinding()]
 param (
     [switch]$Version,
@@ -15,12 +20,21 @@ $dellCommand = $addremovelist | ForEach-Object {Get-ItemProperty $_.PSPath} | Wh
 
 if ($Version){
     if (Test-Path -Path "C:\Program Files (x86)\Dell\CommandUpdate\dcu-cli.exe"){
-        return "Customize"
+        return "Customize x86"
+    }
+    elseif (Test-Path -Path "C:\Program Files\Dell\CommandUpdate\dcu-cli.exe"){
+        return "Customize x64"
     }
     if ($dellCommand){
         $dellCommandVersion = [version]$dellCommand.DisplayVersion
         if (($dellCommandVersion.Major -eq 3) -and ($dellCommandVersion.minor -lt 1)){
             return "Update"
+        }
+        elseif (($dellCommandVersion.Major -eq 3) -and ($dellCommandVersion.minor -eq 1)){
+            if ($dellCommand.InstallLocation -like "*x86*"){
+                return "Customize x86"
+            }
+            return "Customize x64"
         }
     }
     else {
