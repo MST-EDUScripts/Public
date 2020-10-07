cfffa456aa70173a0aad98b5038cb3b68605e320
MST-EDUScripts <63737541+MST-EDUScripts@users.noreply.github.com>
2020-10-07 10:41:48 -0400

    Update Install-PowerManager.ps1
    Converted from .REG file to adding registry entries manually.  Was having issues with "REG IMPORT" command.

--- Dell Command/Install-PowerManager.ps1
+++ Dell Command/Install-PowerManager.ps1
@@ -3,4 +3,14 @@ $downloadURL = "https://dl.dell.com/FOLDER05695265M/3/$download"
 
 Invoke-WebRequest -Uri $downloadURL -OutFile "$PSScriptRoot\$download"
 Start-Process -FilePath "$PSScriptRoot\$download" -ArgumentList "/s" -Wait
-Start-Process REG -ArgumentList "IMPORT $PSScriptRoot\PowerManagerCool.reg"
+
+New-Item -Path HKLM:\SOFTWARE\Policies\Dell
+New-Item -Path HKLM:\SOFTWARE\Policies\Dell\CommandPowerManager
+New-ItemProperty -Path HKLM:\SOFTWARE\Policies\Dell\CommandPowerManager -Name DoNotRemove -PropertyType String -Value "Null"
+New-ItemProperty -Path HKLM:\SOFTWARE\Policies\Dell\CommandPowerManager -Name TMSettingsGroup -PropertyType String -Value "True"
+New-ItemProperty -Path HKLM:\SOFTWARE\Policies\Dell\CommandPowerManager -Name TMPolicyVersion -PropertyType String -Value "1.0.0"
+New-ItemProperty -Path HKLM:\SOFTWARE\Policies\Dell\CommandPowerManager -Name TMMode -PropertyType DWORD -Value 2
+New-ItemProperty -Path HKLM:\SOFTWARE\Policies\Dell\CommandPowerManager -Name TMModeBalanced -PropertyType DWORD -Value 0
+New-ItemProperty -Path HKLM:\SOFTWARE\Policies\Dell\CommandPowerManager -Name TMModeCool -PropertyType DWORD -Value 0
+New-ItemProperty -Path HKLM:\SOFTWARE\Policies\Dell\CommandPowerManager -Name TMModeQuiet -PropertyType DWORD -Value 0
+New-ItemProperty -Path HKLM:\SOFTWARE\Policies\Dell\CommandPowerManager -Name TMModeUltra -PropertyType DWORD -Value 0
